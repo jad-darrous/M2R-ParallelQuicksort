691096ee3b2b01c8cc01b0ad221208ea118405a8
Jad Darrous <jad.darrous@gmail.com>
2016-01-13 12:42:41 +0100

    add more graphs

--- scripts/analysis.R
+++ scripts/analysis.R
@@ -1,15 +1,16 @@
 
 # setwd("/home/jad/dev/M2R-ParallelQuicksort/scripts/n2");  csv_file = "experiments_results.csv";
-csv_file = "/home/jad/dev/M2R-ParallelQuicksort/data/jad-K52Jc_2016-01-10_13:44/experiments_results.csv";
+csv_file = "/home/jad/dev/M2R-ParallelQuicksort/data/jad-K52Jc_2016-01-13_07:46/experiments_results.csv";
 nb_threads = 8
 
 args <- commandArgs(trailingOnly = TRUE)
-if (length(args) != 3) {
-  stop("usage: Rscript analysis.R <csv_file> <out_dir> <nb_threads>", call.=FALSE)
+if (length(args) != 4) {
+  stop("usage: Rscript analysis.R <csv_file> <csv_file_2> <out_dir> <nb_threads>", call.=FALSE)
 }
 csv_file = args[1]
-out_dir = args[2]
-nb_threads = args[3]
+csv_file_2 = args[2]
+out_dir = args[3]
+nb_threads = args[4]
 rm(args)
 
 # =================================================
@@ -34,13 +35,13 @@ library(ggplot2)
 p <- scale_colour_manual("Sorting", values = c("red", "blue", "purple"),
                          labels = c("Sequential", "Parallel", "Libc"))
 
-o <- labs(title = "Mesearments", y = "Time", x = "Size")
+o <- labs(title = "Measurements", y = "Time", x = "Size")
 
 # ggplot(df_size, aes(x=size)) +
 #   geom_point(aes(y=seq), colour="red") +
 #   geom_point(aes(y=par), colour="green") +
 #   geom_point(aes(y=blt), colour="blue") +
-#   labs(title = "Mesearments", y = "Time", x = "Size")
+#   labs(title = "Measurements", y = "Time", x = "Size")
 
 # Same as above but with a legend
 # the Libc data can be removed to get higher resolution
@@ -62,8 +63,14 @@ ggplot(df_size_melt, aes(size, value, colour=variable)) +
   geom_point() +
   p + o
 
-ggsave(paste(out_dir, "size.png", sep="/"), width = 5, height = 5)
+ggsave(paste(out_dir, "size_all.png", sep="/"), width = 5, height = 5)
 
+# Same as above but without the LibC
+ggplot(df_size_melt_par_seq, aes(size, value, colour=variable)) +
+  geom_point() +
+  p + o
+
+ggsave(paste(out_dir, "size_par_seq.png", sep="/"), width = 5, height = 5)
 
 library(plyr)
 
@@ -85,11 +92,24 @@ df_size_melt_par_ci <- compute_ci(df_size_melt, 'par')
 # draw the CI
 ggplot(df_size_melt_par_seq, aes(size, value, colour=variable)) +
   geom_point() +
-  geom_errorbar(data=df_size_melt_par_ci, aes(y=mn,ymin=mn-se,ymax=mn+se)) +
-  geom_errorbar(data=df_size_melt_seq_ci, aes(y=mn,ymin=mn-se,ymax=mn+se)) +
+  geom_errorbar(data=df_size_melt_par_ci, aes(y=mn, ymin=mn-se, ymax=mn+se)) +
+  geom_errorbar(data=df_size_melt_seq_ci, aes(y=mn, ymin=mn-se, ymax=mn+se)) +
   p + o
 
-ggsave(paste(out_dir, "size_ci.png", sep="/"), width = 5, height = 5)
+ggsave(paste(out_dir, "size_ci_points.png", sep="/"), width = 5, height = 5)
+
+
+df_size_melt_par_seq_mn <- ddply(df_size_melt_par_seq, c("size", "variable"), summarize,
+           mn = mean(value));
+
+# draw the CI with the mean
+ggplot(df_size_melt_par_seq, aes(size, value, colour=variable)) +
+  geom_line(data=df_size_melt_par_seq_mn, aes(y=mn, colour=variable)) +
+  geom_errorbar(data=df_size_melt_par_ci, aes(y=mn, ymin=mn-se, ymax=mn+se)) +
+  geom_errorbar(data=df_size_melt_seq_ci, aes(y=mn, ymin=mn-se, ymax=mn+se)) +
+  p + o
+
+ggsave(paste(out_dir, "size_ci_lines.png", sep="/"), width = 5, height = 5)
 
 
 # draw the regression line
@@ -101,13 +121,24 @@ ggplot(df_size_melt, aes(size, value, colour=variable)) +
 ggsave(paste(out_dir, "size_regression.png", sep="/"), width = 5, height = 5)
 
 
+# Same as above but without the LibC
+ggplot(df_size_melt_par_seq, aes(size, value, colour=variable)) +
+  geom_point() +
+  geom_smooth(method = "lm", formula = y ~ x+I(x^2), se = FALSE, size = 1) +
+  p + o
+
+ggsave(paste(out_dir, "size_regression_seq_par.png", sep="/"), width = 5, height = 5)
+
+
 reg_seq <- lm(seq ~ size+I(size^2), df_size)
 
 # reg_par <- lm(par ~ size, df_size)
 reg_par <- lm(par ~ size+I(size^2), df_size)
+reg_par <- lm(par ~ poly(size, 4), df_size)
 reg_par <- lm(par ~ size*I(log(size)), df_size)
 reg_par <- lm(par ~ size+I(log(size)), df_size)
 reg_par <- lm(par ~ size, df_size)
+
 # summary(reg_par)
 
 xv <- seq(100,10^6,10^3)
@@ -120,7 +151,7 @@ pred_y_seq <- predict(reg_seq, list(size=xv))
 
 # draw the regression line in a more complicated way
 ggplot(data=df_size) + theme_bw() +
-  labs(title = "Mesearments", y = "Time", x = "Size") +
+  labs(title = "Measurements", y = "Time", x = "Size") +
   geom_hline(yintercept=0) + geom_vline(xintercept=0) +
   geom_point(aes(x=size,y=par), color="blue", size=2) +
   geom_point(aes(x=size,y=seq), color="red",  size=2) +
@@ -143,52 +174,31 @@ ggplot(df_threads, aes(size, par, color=factor(threads))) +
   geom_point() +
   # formula = y ~ poly(x, 2)
   geom_smooth(method = "lm", size = 1, formula = y ~ x+I(x^2), se = FALSE) +
-  scale_colour_manual("#Threads", values = c("red", "blue", "green", "purple", "yellow"))
+  scale_colour_manual("#Threads", values = c("blue", "red", "purple", "green", "yellow"))
 
 ggsave(paste(out_dir, "threads.png", sep="/"), width = 5, height = 5)
 
+# same as above + regression line, just for 4 and 8
+df_threads_4_8 <- df_threads[(df_threads$threads == 4) | (df_threads$threads == 8),]
+ggplot(df_threads_4_8, aes(size, par, color=factor(threads))) +
+  geom_point() +
+  geom_smooth(method = "lm", size = 1, formula = y ~ poly(x, 2), se = FALSE) +
+  # geom_smooth(method = "lm", size = 1, formula = y ~ x+I(x^2), se = FALSE) +
+  scale_colour_manual("#Threads", values = c("red", "purple"))
 
-# =================================================
-# =================================================
-# =================================================
-# =================================================
+ggsave(paste(out_dir, "threads_4_8_M.png", sep="/"), width = 5, height = 5)
 
-#
-# reg = reg_par
-# summary(reg)
-# plot(reg)
-# par(mfrow=c(2,2));     plot(reg);     par(mfrow=c(1,1))
-#
-# wireframe(seq ~ size * threads, data=allexp_df)
-# wireframe(par ~ size * threads, data=allexp_df)
-#
-# p <- wireframe(par ~ size * threads, data=allexp_df)
-# npanel <- c(4, 2)
-# rotx <- c(-50, -80)
-# rotz <- seq(30, 300, length = npanel[1]+1)
-# update(p[rep(1, prod(npanel))], layout = npanel,
-#        panel = function(..., screen) {
-#          panel.wireframe(..., screen = list(par = rotz[current.column()],
-#                                             size = rotx[current.row()]))
-#        })
-#
-#
-# ggplot(df_size, aes(x=size)) + scale_x_log10() +
-#   geom_line(aes(y=res), colour="red")  +
-#   geom_point(aes(y=seq), colour="red") +
-#   geom_point(aes(y=par), colour="green") +
-#   geom_point(aes(y=blt), colour="blue") + ylab("Time") + xlab("size")
-#
-#
-#
-# ggplot(df_thread_M, aes(x=threads))  + scale_x_discrete(limits=c(1, 2, 4, 8, 16, 32)) +
-#   geom_line(aes(y=seq), colour="red")  +
-#   geom_line(aes(y=par), colour="green")  +
-#   geom_line(aes(y=blt), colour="blue")  + ylab("Time") + xlab("size")
+# read the second expr.
+df_tr48 <- read.csv(csv_file_2, sep=" ", header=T, strip.white=TRUE)
 
+# renaming for simplicity
+names(df_tr48)[3] <- c("par")
 
-# library(plyr)
-#
-# aggr_df <- ddply(allexp_df, c("size", "threads"), summarize,
-#            seq = mean(sequential_time), par = mean(parallel_time), blt = mean(builtin_time),
-#            seq2 = seq^2, par2 = par^2, blt2 = blt^2);
+qplot (size, par, data=df_tr48, color=factor(threads))
+
+ggplot(df_tr48, aes(size, par, color=factor(threads))) +
+  geom_point() +
+  geom_smooth(method = "lm", size = 1, formula = y ~ poly(x, 2), se = FALSE) +
+  scale_colour_manual("#Threads", values = c("red", "purple"))
+
+ggsave(paste(out_dir, "threads_4_8_10M.png", sep="/"), width = 5, height = 5)
