0195ee7032496b3e1498d868a9b16efb6f265dda
Jad Darrous <jad.darrous@gmail.com>
2016-01-13 12:39:42 +0100

    adds support for the second experiment and pass parameters to the make

--- build.sh
+++ build.sh
@@ -1,26 +1,41 @@
 
-make -C src/ clean
-make -C src/
-
+# global variables
 EXPERIMENTS_DIR="data"
 OUT_DIR_NAME=`hostname`_`date +%F`_`date +%R | xargs`
 
 OUTPUT_DIRECTORY="${EXPERIMENTS_DIR}/${OUT_DIR_NAME}"
 mkdir -p $OUTPUT_DIRECTORY
+
 EXPR_FILE="${OUTPUT_DIRECTORY}/experiments.csv"
+EXPR_FILE_2="${OUTPUT_DIRECTORY}/experiments_big.csv"
+
 RESULT_FILE="${OUTPUT_DIRECTORY}/experiments_results.csv"
+RESULT_FILE_2="${OUTPUT_DIRECTORY}/experiments_big_results.csv"
+
 FINAL_RESULTS_DIR="${EXPERIMENTS_DIR}/last"
 
 NB_THREADS=8
 
+
 # generate the DoE
-Rscript ./scripts/gen_experiments.R $EXPR_FILE
+Rscript ./scripts/gen_experiments.R $EXPR_FILE $EXPR_FILE_2
+
+# make sure that the binaries are up-to-date
+make -C src/ clean
+make -C src/ ARGS="-DALL_IMPL"
 
 # run the experiments
 python ./scripts/run_experiments.py $EXPR_FILE $RESULT_FILE
 
+# make sure that the binaries are up-to-date
+make -C src/ clean
+make -C src/ ARGS=""
+
+# run the second experiments
+python ./scripts/run_experiments.py $EXPR_FILE_2 $RESULT_FILE_2
+
 # analyze the results
-Rscript ./scripts/analysis.R $RESULT_FILE $OUTPUT_DIRECTORY $NB_THREADS
+Rscript ./scripts/analysis.R $RESULT_FILE $RESULT_FILE_2 $OUTPUT_DIRECTORY $NB_THREADS
 
 # move the generated pdf to the output directory
 mv Rplots.pdf $OUTPUT_DIRECTORY
